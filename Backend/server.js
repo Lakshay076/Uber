344cb88fb07d35130aec3ad4b8c4cbee4f99bd8c
Lakshay076 <154418550+Lakshay076@users.noreply.github.com>
2025-04-02 16:24:20 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv')
+dotenv.config();
+const http = require('http');
+const cors = require('cors');
+const app = require('./app');
+const port = process.env.POST || 3000;
+
+app.use(cors());
+
+
+const server = http.createServer(app);
+
+
+server.listen(port, ()=> {
+    console.log(`Server is running on port ${port}`);
+});
